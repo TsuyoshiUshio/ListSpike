3e6838e49b568f410c6ce52d174cbb720ae4648c
Tsuyoshi Ushio <ushio@simplearchitect.com>
2018-06-01 00:35:51 +0900

    Initial Commit

--- SampleString/Program.cs
+++ SampleString/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace SampleString
 {
@@ -6,7 +10,79 @@ namespace SampleString
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            var InstancesControllerSegment = "/instances/";
+            var path = "/admin/extensions/DurableTaskExtension/instances/";
+            int i = path.IndexOf(InstancesControllerSegment, StringComparison.OrdinalIgnoreCase);
+
+            i += InstancesControllerSegment.Length;
+            int nextSlash = path.IndexOf('/', i);
+            string instanceId = path.Substring(i);
+            if(instanceId == string.Empty)
+            {
+                Console.WriteLine($"Empty instanceId: {instanceId}");
+            }
+            TestAddList();
+            Console.ReadLine();
         }
+
+        public class SomeClass
+        {
+            public string Name { get; set; }
+            public int Age { get; set; }
+        }
+
+        public static void TestAddList()
+        {
+            var number = 100000000;
+            // Add 10000 instances to the List. 
+            var watch = new Stopwatch();
+            watch.Start();
+
+            var list = new List<SomeClass>(number);
+            list.Add(
+                new SomeClass
+                {
+                    Name = "ushio",
+                    Age = 47
+                }
+                );
+            watch.Stop();
+            Console.WriteLine($"List With number constructor: {watch.ElapsedMilliseconds}");
+            watch.Restart();
+            list = new List<SomeClass>(number);
+            list.Add(
+                new SomeClass
+                {
+                    Name = "ushio",
+                    Age = 47
+                });
+            watch.Stop();
+            Console.WriteLine($"List without constructor: {watch.ElapsedMilliseconds}");
+            RunoutThreading().GetAwaiter().GetResult();
+    
+        }
+
+        private static async Task SomeExecAsync()
+        {
+            // Console.WriteLine($"SomeExecAsync(): Current: {Thread.CurrentThread.ManagedThreadId}");
+            await Task.Delay(1000 * 3);
+        }
+
+        public static async Task RunoutThreading()
+        {
+            var watch = new Stopwatch();
+            watch.Start();
+            var number = 100000;
+            var list = new List<Task>(number);
+            for(var i = 0; i < number; i++)
+            {
+                list.Add(SomeExecAsync());
+            }
+            watch.Stop();
+            Console.WriteLine($"RunoutThread finished: elapse : {watch.ElapsedMilliseconds}");
+            await Task.WhenAll(list);
+        }
+
     }
 }
